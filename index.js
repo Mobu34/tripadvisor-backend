45e3a7ed7a2acabc9d8326f0f735dc304e19c372
Lucas Tidas <Lucas@pc-225.home>
2020-10-31 14:14:59 +0100

    dotenv added

--- index.js
+++ index.js
@@ -73,6 +73,6 @@ app.all("*", (req, res) => {
   res.status(404).json("Page not found");
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT, () => {
   console.log("Server has started ...");
 });
